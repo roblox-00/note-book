64a0ff38b564dd57086b3d460d4ec77cd030b6f5
roblox-00 <48410833+roblox-00@users.noreply.github.com>
2019-06-03 17:22:36 +0430

    Initial commit

---
+++ app/src/main/java/test/app/note/tajik/zakaria/CustomAdapterNotes.java
@@ -0,0 +1,39 @@
+package test.app.note.tajik.zakaria;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CustomAdapterNotes extends ArrayAdapter<Note>{
+
+    ArrayList<Note> notes;
+    public CustomAdapterNotes(Context context, ArrayList<Note> notes) {
+        super(context, R.layout.note_layout);
+        this.notes = notes;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        @SuppressLint("ViewHolder") View custom_view = inflater.inflate(R.layout.note_layout, parent, false);
+        TextView date = custom_view.findViewById(R.id.date);
+        TextView note = custom_view.findViewById(R.id.main_text);
+
+        note.setText(notes.get(position).getNote_text());
+        date.setText( notes.get(position).getNote_date());
+
+        return custom_view;
+    }
+
+    @Override
+    public int getCount() {
+        return notes.size();
+    }
+
+
+}
---
+++ app/src/main/java/test/app/note/tajik/zakaria/Student.java
@@ -0,0 +1,28 @@
+package test.app.note.tajik.zakaria;
+
+public class Student {
+    private String name="";
+    private String gpa="";
+
+    public Student(String name, String gpa) {
+        this.name = name;
+        this.gpa = gpa;
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGpa(String gpa) {
+        this.gpa = gpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGpa() {
+        return gpa;
+    }
+}
